b3f3fc973c2d5872ba6428fb401d2cb537f35726
Ji-Yuhang <yuhang.silence@gmail.com>
2026-03-20 20:28:04 +0800

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/shooting-game/" : "/",
   server: {
     host: "0.0.0.0",
     port: 5173
   }
-});
+}));
